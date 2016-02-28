96c7e1e86d6dd4a333ef38bec24eba2bad50a534
Raghu Reddy <raghureddy.ram@gmail.com>
2016-02-27 18:42:27 -0800

    Basic logging

--- snippets.py
+++ snippets.py
@@ -3,21 +3,26 @@ import logging
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 
-def put(name, snipppet):
-	"""
-	Store a snippet with an associated name.
-
-    Returns the name and the snippet.
+def put(name, snippet):
     """
-    logging.error("FIXME: Uninmplemented - put({!r}, {!r})".format(name, snippet))
+    Store a snippet with an associated name.
+
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
     return name, snippet
 
-def get(name, snippet):
+def get(name):
 	"""Retrieve the snippet with a given name.
 
-    If there is no such snippet, return '404: Snippet Not Found'.
+	If there is no such snippet, return '404: Snippet Not Found'.
 
-    Returns the snippet.
-    """
-    logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
+	Returns the snippet.
+	"""
+	logging.error("FIXME: Unimplemented - get({!r})".format(name))
+	return ""
+
+
+put("list", "A sequence of things - created using []")
+
+get("list")
