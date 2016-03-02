310b6155ec3a9a36396e870d6356274d6704f005
Raghu Reddy <raghureddy.ram@gmail.com>
2016-03-02 01:53:10 -0800

    Get function not working, syntax issue

--- snippets.py
+++ snippets.py
@@ -6,6 +6,8 @@ logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 logging.debug("Connecting to PostgreSQL")
 connection = psycopg2.connect(database="snippets")
 logging.debug("Database connection established.")
+cursor = connection.cursor()
+
 
 def put(name, snippet):
     """Store a snippet with an associated name."""
@@ -18,14 +20,18 @@ def put(name, snippet):
     return name, snippet
 
 def get(name):
-	"""Retrieve the snippet with a given name.
+    """Retrieve the snippet with a given name.
+    If there is no such snippet, return '404: Snippet Not Found'.
+    Returns the snippet.
+    """
+    cursor = connection.cursor()
+    command = 'SELECT keyword FROM snippets WHERE keyword = %s;'
+    cursor.execute(command, (name))
 
-	If there is no such snippet, return '404: Snippet Not Found'.
-
-	Returns the snippet.
-	"""
-	logging.error("FIXME: Unimplemented - get({!r})".format(name))
-	return ""
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    value = cursor.fetchnone()
+    print(value)
+    return value
 
 def main():
     """Main function"""
@@ -38,21 +44,25 @@ def main():
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
+
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="Name of the snippet")
+
     arguments = parser.parse_args()
     # Convert parsed arguments from Namespace to dictionary
-    arguments = vars(arguments) ## how to use vars? vars() or vars()[].. seen both
+    arguments = vars(arguments)
 
     ## is vars(name="list", snippet="A sequence of things - created using []") syntactic sugar for a dictionary created from optional args?
     command = arguments.pop("command")
 
     if command == "put":
-    	name, snippet = put(**arguments)
-    	print("Stored {!r} as {!r}".format(snippet, name))
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
     elif command == "get":
-    	snippet = get(**arguments) ## what's the diff between * vs **?
-    	print("Retrieved snippet: {!r}".format(snippet))
+        # snippet = get(*arguments) ## what's the diff between * vs **?
+
+        name = arguments.pop('name')
+        print("Retrieved snippet: {!r}".format(get(name)))
 
 if __name__ == "__main__":
 	main()
